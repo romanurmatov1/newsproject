982d5303a91d9e1a9d1c723e52ca6c3ba135c4ef
raxmatjon <82531443+romanurmatov1@users.noreply.github.com>
2022-06-09 00:42:09 +0500

    first commit

---
+++ news/models.py
@@ -0,0 +1,10 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150);
+    text = models.TextField();
+
+    def __str__(self):
+        return self.title
